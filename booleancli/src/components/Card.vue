1008ca5c6b28d9100b39af4da251bad79e0ebb52
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-12 19:19:16 +0100

    Esercizio

---
+++ booleancli/src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div class="ratio">
+      <div class="content">
+        <img class="card__img" :src="thumbnail" alt="" />
+      </div>
+    </div>
+    <p class="card__text">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    thumbnail: String,
+    title: String,
+  },
+};
+</script>
+
+<style >
+
+</style>
--- booleancli/src/components/PageMain.vue
+++ booleancli/src/components/PageMain.vue
@@ -1,13 +1,30 @@
 <template>
-<div class="container_main">
-  <h1>Content goes here</h1>
-
-</div>
-
+<main class="container_main">
+  <div class="container">
+  <div class="cards_container">
+  <Card 
+  v-for="(card, i) in cardsArray" :key="i"
+  :thumbnail="card.thumb"
+  :title="card.series"
+  />
+  </div>
+  </div>
+</main>
 </template>
 
 <script>
+import JsonData from "@/assets/datajsn/dc-comics.json";
+import Card from "./Card.vue";
+
  export default {
      name: "PageMain",
- }
+     components:{
+       Card,
+     },
+     data(){
+       return{
+         cardsArray: JsonData,
+       };
+     },
+ };
 </script>
